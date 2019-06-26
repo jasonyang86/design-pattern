3a6771d086b9dfd07dbd8eaa57b89f91635eedba
yangjinlong <yangjinlong86@gmail.com>
2019-06-26 11:42:27 +0800

    update readme

--- src/main/java/org/nocoder/observer/Demo.java
+++ src/main/java/org/nocoder/observer/Demo.java
@@ -2,10 +2,10 @@ package org.nocoder.observer;
 
 public class Demo {
     public static void main(String[] args) {
-        // 被观察者
+        // 被观察者，发布消息者
         Subject subject = new Subject();
 
-        // 观察者
+        // 观察者，订阅者消费消息
         new BinaryObserver(subject);
         new OctalObserver(subject);
         new HexaObserver(subject);
